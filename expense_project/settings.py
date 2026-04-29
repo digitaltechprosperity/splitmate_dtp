4ec3ab714d4c388b6b3104faa36845aa1587ee51
srujan <srujan@gmail.com>
2026-04-30 00:09:03 +0530

    Use console email backend for testing

--- expense_project/settings.py
+++ expense_project/settings.py
@@ -148,7 +148,7 @@ if not DEBUG:
 # =========================
 # EMAIL CONFIG
 # =========================
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
